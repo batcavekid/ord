35002835c95765d86bdae8e576388993d03d5cae
batcavekid <clickitback@gmail.com>
2022-12-28 03:56:57 +1300

    Update options.rs

--- src/options.rs
+++ src/options.rs
@@ -119,11 +119,11 @@ impl Options {
   pub(crate) fn bitcoin_rpc_client_mainnet_forbidden(&self, command: &str) -> Result<Client> {
     let client = self.bitcoin_rpc_client()?;
 
-//    if self.chain() == Chain::Mainnet {
+    if self.chain() == Chain::Mainnet {
 //      bail!("`{command}` is unstable and not yet supported on mainnet.");
 //    }
-//    Ok(client)
-//  }
+    Ok(client)
+  }
 
   pub(crate) fn bitcoin_rpc_client_for_wallet_command(&self, command: &str) -> Result<Client> {
     let client = self.bitcoin_rpc_client()?;
